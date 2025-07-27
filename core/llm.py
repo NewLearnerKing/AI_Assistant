9e4df27f5358e0b6e3a8efa6e72d0d43f6c011fa
Abhishek Patel <abhishekjpatel1400@gmail.com>
2025-07-27 22:55:41 +0530

    Fixed minor bug of API call

--- core/llm.py
+++ core/llm.py
@@ -7,6 +7,10 @@ import pytz
 
 from core.memory_manager import MemoryManager
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-2.0-flash")
 memory = MemoryManager()
